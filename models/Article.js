948e2f28b259c162a77b467d5bbae6b236e5be8b
KadyrPoyraz228337 <poyraz_2001@mail.ru>
2020-07-07 20:28:53 +0600

    Добавил модель статьи Article.js

---
+++ models/Article.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const ArticleSchema = mongoose.Schema({
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: String
+})
+
+const modelName = 'Article';
+
+module.exports = mongoose.model(modelName, ArticleSchema)
