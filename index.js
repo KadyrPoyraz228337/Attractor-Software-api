3ef5bfa93a440499314e2af98abb8a8b97764678
KadyrPoyraz228337 <poyraz_2001@mail.ru>
2020-07-07 13:23:22 +0600

    Добавил index.js с подключением к mongoDB базе

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const config = require('./config');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+
+const run = async () => {
+    await mongoose.connect(config.database, config.databaseOptions);
+
+    app.listen(config.port)
+};
+
+run().catch(e => {
+    console.error(e)
+});
