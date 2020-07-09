37a5bdab84e272aaac0899b3c2ab78255b92faf0
KadyrPoyraz228337 <poyraz_2001@mail.ru>
2020-07-09 13:13:25 +0600

    Добавил fixtures.js и скрипт для запуска - "seed"

---
+++ fixtures.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const config = require("./config");
+
+const User = require('./models/User');
+
+const run = async () => {
+    await mongoose.connect(config.database, config.databaseOptions);
+
+    const collection = await mongoose.connection.db.listCollections().toArray();
+
+    for (let coll of collection) {
+        await mongoose.connection.db.dropCollection(coll.name);
+    }
+
+    const [valera, alex, john] = await User.create({
+        username: 'Valera',
+        password: '123',
+        token: '123'
+    },{
+        username: 'Alex',
+        password: '123',
+        role: 'admin',
+        token: '123'
+    },{
+        username: 'john',
+        password: '123',
+        token: '123'
+    })
+
+    mongoose.connection.close();
+};
+
+run().catch(e => {
+    throw e;
+});
